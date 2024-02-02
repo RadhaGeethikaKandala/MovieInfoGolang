6685d18b74bcf4f3b3783fafcaeb49d99d2a041c
Arkaprabha Chatterjee <arkaprabha.chatterjee31@gmail.com>
2024-02-02 18:39:23 +0530

    Added more db data

--- internal/app/database/migrations/000002_seed_database.up.sql
+++ internal/app/database/migrations/000002_seed_database.up.sql
@@ -2,8 +2,10 @@ INSERT INTO ratings (source, value)
 VALUES
   ('Internet Movie Database', '8.2/10'),
   ('Rotten Tomatoes', '85%'),
-  ('Metacritic', '70/100');
-
+  ('Metacritic', '70/100'),
+  ('Internet Movie Database', '7.8/10'),
+  ('Rotten Tomatoes', '85%'),
+  ('Metacritic', '72/100');
 
 
 INSERT INTO movies (
@@ -25,14 +27,30 @@ VALUES
     , '70', '8.2', '1,559,730'
     , 'tt0372784', 'movie'
     , '09 Sep 2009', '$206,863,479'
+    , 'N/A', 'N/A', 'true'),
+  ('The Batman', '2022', 'PG-13', '04 Mar 2022'
+    , '176 min', 'Action, Crime, Drama', 'Matt Reeves'
+    , 'Matt Reeves, Peter Craig, Bob Kane'
+    , 'Robert Pattinson, Zoë Kravitz, Jeffrey Wright'
+    , 'When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the citys hidden corruption and question his familys involvement.'
+    , 'English, Spanish, Latin, Italian'
+    , 'United States'
+    , 'Nominated for 3 Oscars. 35 wins & 171 nominations total'
+    , 'https://m.media-amazon.com/images/M/MV5BM2MyNTAwZGEtNTAxNC00ODVjLTgzZjUtYmU0YjAzNmQyZDEwXkEyXkFqcGdeQXVyNDc2NTg3NzA@._V1_SX300.jpg'
+    , '72', '7.8', '765,024'
+    , 'tt1877830', 'movie'
+    , '19 Apr 2022', '$369,345,583'
     , 'N/A', 'N/A', 'true');
+
 
 
 INSERT INTO moviesratings ( movie_id , rating_id )
 VALUES
   (1,1),
   (1,2),
-  (1,3);
-
+  (1,3),
+  (2,4),
+  (2,5),
+  (2,6);
 
 
