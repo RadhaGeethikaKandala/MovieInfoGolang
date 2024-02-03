12137b8e787b81bc6cf0c2f15d8a9aade0f894ec
Arkaprabha Chatterjee <arkaprabha.chatterjee31@gmail.com>
2024-02-03 11:58:33 +0530

    Removed imdbid from MoviesRequest struct

--- internal/app/dto/request/movies_request.go
+++ internal/app/dto/request/movies_request.go
@@ -1,7 +1,6 @@
 package request
 
 type MoviesRequest struct {
-	ImdbId string `json:"imdbid" form:"imdbid"`
 	Genre  string `json:"genre" form:"genre"`
 	Actors string `json:"actors" form:"actors"`
 	Year   string `json:"year" form:"year"`
