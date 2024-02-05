c9b62ed4de50aaef0b09302d85241a8157d6d666
radha.kandala <radha.kandala@thoughtworks.com>
2024-02-05 12:49:46 +0530

    Arkaprabha | refactor config file

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -6,36 +6,40 @@ import (
 	"github.com/spf13/viper"
 )
 
-
 type Config struct {
-	Server ServerConfig
+	Server   ServerConfig
 	Database DBConfig
 }
 type ServerConfig struct {
 	Host string
 	Port string
-
 }
 type DBConfig struct {
 	Username string
 	Password string
-	Dbname string
-	Host string
-	Port string
-	Sslmode string
+	Dbname   string
+	Host     string
+	Port     string
+	Sslmode  string
 }
+
+const (
+	configPath = "./internal/app/config"
+	configName = "config"
+	configType = "yaml"
+)
 
 func ReadConfig() *Config {
 	var config = &Config{}
-	viper.AddConfigPath("./internal/app/config")
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
+	viper.AddConfigPath(configPath)
+	viper.SetConfigName(configName)
+	viper.SetConfigType(configType)
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Unable to read config file. Error : %s", err.Error())
 	}
 
-	err := viper.Unmarshal(config);
+	err := viper.Unmarshal(config)
 	if err != nil {
 		log.Fatalf("Error in unmarshalling config : %s", err.Error())
 	}
